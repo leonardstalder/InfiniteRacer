06d0ccf404302def4a02566259c955de2201668f
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2013-11-05 07:42:57 +0100

    Added loading screen
    To be further modified

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+	
+	public Texture loadingTexture;
+	
+	void Awake()
+	{
+		Time.timeScale = 0;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI () 
+	{
+		// Put background image.
+		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height), loadingTexture, ScaleMode.StretchToFill);
+
+		if(GUI.Button (new Rect (Screen.width/2 - 125, Screen.height/2 -20,250,30), "Press Enter"))
+		{
+			Time.timeScale = 1;
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour {
+	
+	// Variables.
+	private int loading = 1;
+	public Texture loadingTexture;
+	public float loadingState;
+	
+	void Start() 
+	{
+        Application.LoadLevelAsync("MainScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI () 
+	{
+		// Put background image.
+		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height), loadingTexture, ScaleMode.StretchToFill);
+	}
+}
